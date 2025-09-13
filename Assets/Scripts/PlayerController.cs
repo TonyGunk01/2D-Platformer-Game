04a46a10ebd2e8b824bed1b2320a281b29ef0bbd
TonyGunk01 <rishivl@hotmail.com>
2025-09-13 16:16:14 +0530

    adding rigidbody components

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("PlayerController Awake");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision: " + collision.gameObject.name);
+    }
+}
